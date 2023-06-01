b83de8a4bcfb63d441d8992582a3d47aeff214fa
kael.moreira <kael.moreira@elentra.com>
2023-06-01 16:23:11 -0400

    finished creating command to linux

--- diff_tool.py
+++ diff_tool.py
@@ -1,3 +1,4 @@
+import argparse
 import difflib
 import sys
 
@@ -10,3 +11,18 @@ def create_diff(old_file: Path, new_file:Path):
 
     delta = difflib.unified_diff(file1, file2, old_file, new_file)
     sys.stdout.writelines(delta)
+
+def main():
+    parser = argoarse.ArgumentParser()
+    parser.add_argument('old_file_version')
+    parser.add_argument('new_file_version')
+    args = parser.parse_args()
+
+    old_file = Path(args.old_file_version)
+    new_file = Path(args.new_file_version)
+
+    create_diff(old_file, new_file)
+
+
+if __name__ == '__main__':
+    main()
