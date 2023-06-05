de323b98b0b9cf107c0ed66ff8870186ca529d04
kael.moreira <kael.moreira@elentra.com>
2023-06-05 12:01:23 -0400

    better tool

--- diff_tool.py
+++ diff_tool.py
@@ -1,29 +1,28 @@
 import argparse
 import difflib
-import sys
-
 from pathlib import Path
 
 
 def create_diff(old_file: Path, new_file: Path, html_output: Path = None):
-    file1 = open(old_file, encoding='utf-8').readlines()
-    file2 = open(new_file, encoding='utf-8').readlines()
+    file1_lines = old_file.read_text(encoding='utf-8').splitlines()
+    file2_lines = new_file.read_text(encoding='utf-8').splitlines()
+
+    differ = difflib.HtmlDiff()
+    delta = differ.make_file(file1_lines, file2_lines, str(old_file), str(new_file))
 
     if html_output:
-        delta = difflib.HtmlDiff().make_file(
-            file1, file2, old_file, new_file)
         with open(html_output, 'w', encoding='utf-8') as f:
             f.write(delta)
+        print(f"Differences between the files have been saved to {html_output}.")
     else:
-        delta = difflib.unified_diff(file1, file2, old_file, new_file)
-        sys.stdout.writelines(delta)
+        print(delta)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("old_file_version")
     parser.add_argument("new_file_version")
-    parser.add_argument("--html", help="Specify the html output file")
+    parser.add_argument("--html", help="Specify the HTML output file")
     args = parser.parse_args()
 
     old_file = Path(args.old_file_version)
